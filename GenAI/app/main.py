f16ddc791777a2ffbc42a612fb6688640cc62350
Nityam-Tripathi <nityamtripathi02@gmail.com>
2026-04-14 18:38:58 +0530

    db added

--- GenAI/app/main.py
+++ GenAI/app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from .database import engine
+from .models import Base
 
 app = FastAPI()
 
+# Create tables
+Base.metadata.create_all(bind=engine)
+
 @app.get("/")
 def root():
-    return {"message": "Backend is running 🚀"}
+    return {"message": "Backend + DB working ✅"}
 
 @app.get("/test")
 def test():
